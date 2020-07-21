bd23d4bcfd5a8728c85f18298a1c43578bde5bac
Dustin Larimer <180438+dustinlarimer@users.noreply.github.com>
2020-07-21 15:49:07 -0700

    Update styles

--- pages/_app.js
+++ pages/_app.js
@@ -10,11 +10,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Root System</title>
-        <meta 
-          name="Description" 
-          content="We help startup founders de-risk their product/technology by designing & building high-quality software and high-functioning engineering organizations." 
-        />
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700;900&display=swap" rel="stylesheet" />
+        <meta name="Description" content="We help startup founders de-risk their product/technology by designing & building high-quality software and high-functioning engineering organizations." />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700;900&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&display=swap" rel="stylesheet" />
         {/* <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}

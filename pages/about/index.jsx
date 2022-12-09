c81bf9fd88ffb1787f41d1993a0691b39e6d127d
Kyle Wild <kyle@kylewild.com>
2022-12-09 10:19:33 -0800

    Update index.jsx

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -27,17 +27,17 @@ export default function AboutPage ({ }) {
           px={{ base: 10, sm: 0 }}
           textAlign="center"
         >
-          A new kind of consulting firm
+          A new kind of venture firm.
         </Heading>
         <Box lineHeight={7} maxWidth="lg" mx="auto">
           <Text mb={4}>
-            We are a group of veteran founders, CTOs, developers, and product designers.  We do consulting for early-stage startups, and we work at steeply discounted rates in exchange for stock in the companies we serve. This "skin in the game" means we are incentivized to do a <em>very</em> good job.
+            Root System is a collective of veteran startup founders, growth hackers, CTOs, developers, and product leaders. We do consulting for early-stage startups, and we work at steeply discounted rates in exchange for stock in the companies we serve.  This "skin in the game" means we are incentivized to do a <em>very</em> good job.
           </Text>
           <Text mb={4}>
             We help startup founders de-risk their products and technology by designing &amp; building high-quality software and high-functioning engineering organizations. We also help those founders develop markets, raise capital, and recruit + mentor full-time staff.
           </Text>
           <Text mb={4}>
-            You can think of Root System as a seed stage venture firm, but one that invests sweat and brainpower and code (which are highly differentiated) instead of investing cash (which is not); or if you prefer, think of us as an accelerator for your product itself.
+            You can think of Root System as a seed stage venture firm, but one that invests sweat and brainpower and code (which are highly differentiated) instead of investing cash (although we do that too);  or if you prefer, think of us as an accelerator for your product itself.
           </Text>
           <Text mb={12}>
             <RouterLink href="/contact">
@@ -48,7 +48,7 @@ export default function AboutPage ({ }) {
               >
                 Get in touch
               </Link>
-            </RouterLink>
+            </RouterLink> or just email us, partners@rootsystem.com
           </Text>
         </Box>
       </Box>

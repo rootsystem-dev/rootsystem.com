153c89fa115f35be3539ee537ef8e7aa32098f8e
Jeff <jeff.latz@gmail.com>
2024-02-27 12:16:22 -0500

    change this back, add env variable in netlify GUI

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const baseConfig = (phase, { defaultConfig }) => {
   return {
+    output: 'export',
     poweredByHeader: false,
     webpack: (config, { isServer }) => {
       config.module.rules.push({
